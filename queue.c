1ad40c66bdb192dd993e956ecba93559fddd7e3f
Chiu-Po-Ying <charlie910417@gmail.com>
2025-02-25 22:00:46 +0800

    Implement queue inserting function
    Add the function that inserts the element at head or tail of the queue.
    The function initializes the element, then add it into the queue.It
    should return false if there is a memory allocation error, the input
    pointer is empty or string copy error.
    
    Change-Id: I1b6e83b36ac7e05a4188a1902ca1d60d683c4d1e

--- queue.c
+++ queue.c
@@ -40,12 +40,50 @@ void q_free(struct list_head *head)
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head || !s) {
+        return false;
+    }
+
+    element_t *new_content = malloc(sizeof(element_t));
+
+    if (!new_content) {
+        return false;
+    }
+
+    INIT_LIST_HEAD(&new_content->list);
+    new_content->value = strdup(s);
+
+    if (!new_content->value) {
+        free(new_content);
+        return false;
+    }
+
+    list_add(&new_content->list, head);
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head || !s) {
+        return false;
+    }
+
+    element_t *new_content = malloc(sizeof(element_t));
+
+    if (!new_content) {
+        return false;
+    }
+
+    INIT_LIST_HEAD(&new_content->list);
+    new_content->value = strdup(s);
+
+    if (!new_content->value) {
+        free(new_content);
+        return false;
+    }
+
+    list_add_tail(&new_content->list, head);
     return true;
 }
 
