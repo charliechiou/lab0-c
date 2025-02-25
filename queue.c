9dd751c73105985006ea992f914d7cdcbb6483ca
Chiu-Po-Ying <charlie910417@gmail.com>
2025-02-25 21:56:57 +0800

    Implement q_free function
    Add the q_free function that free the memory of the queue.The function
    list through the queue by using list_for_each_safe function then free
    each node.
    
    Change-Id: I2502ef61a6f899f8320a55e928e1e7a87448b31e

--- queue.c
+++ queue.c
@@ -20,7 +20,22 @@ struct list_head *q_new()
 
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head) {
+        return;
+    }
+
+    struct list_head *node, *safe;
+
+    list_for_each_safe (node, safe, head) {
+        list_del(node);
+        free(list_entry(node, element_t, list)->value);
+        free(list_entry(node, element_t, list));
+    }
+    free(head);
+}
+
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
