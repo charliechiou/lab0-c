31438b2b402b061a37b18c3d0e1f2328d963a94d
Chiu-Po-Ying <charlie910417@gmail.com>
2025-02-27 00:16:33 +0800

    Fix q_remove_tail and q_remove_head
    Fixed the error at q_remove_tail and q_remove_head.The previous function
    check whether input of character pointer is empty at the start of the
    function.However this will cause the function not remove the node
    when input is setting wrong.Therefore, I move the checking after the
    list delete function.
    
    Change-Id: I93e0ab7530a1cc6dbecfed444ac2e714eb7cef29

--- queue.c
+++ queue.c
@@ -90,12 +90,16 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    if (!head || list_empty(head) || !sp) {
+    if (!head || list_empty(head)) {
         return NULL;
     }
 
     element_t *first = list_first_entry(head, element_t, list);
     list_del(&first->list);
+
+    if (!sp) {
+        return NULL;
+    }
 
     size_t len = strlen(first->value);
     size_t copy_size = (len < bufsize - 1) ? len : (bufsize - 1);
@@ -109,12 +113,16 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    if (!head || list_empty(head) || !sp) {
+    if (!head || list_empty(head)) {
         return NULL;
     }
 
     element_t *tail = list_last_entry(head, element_t, list);
     list_del(&tail->list);
+
+    if (!sp) {
+        return NULL;
+    }
 
     size_t len = strlen(tail->value);
     size_t copy_size = (len < bufsize - 1) ? len : (bufsize - 1);
